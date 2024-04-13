ae54ac85911a079cabe4186b7880866fe622a33b
MMorales <mmorales@iparksa.com>
2024-04-13 07:28:23 +0200

    censor word

--- Program.cs
+++ Program.cs
@@ -6,8 +6,15 @@ namespace Logica
     {
         static void Main(string[] args)
         {
-            Stairs.PrintStairs(8);
+            string phrase = "En este vasto mundo, cada rincón es un lugar único lleno de misterios. Explorar el mundo es sumergirse en una aventura sin fin, donde cada lugar guarda secretos sorprendentes. El mundo es nuestro hogar, un lugar maravilloso lleno de diversidad y belleza. No importa dónde vayas, siempre encontrarás algo nuevo por descubrir en este increíble mundo. Cada cultura, cada paisaje, cada lugar contribuye a la increíble variedad que hace que nuestro mundo sea tan fascinante. Así que sal y explora este maravilloso mundo, porque cada lugar que visites te llevará a una nueva experiencia y te recordará la grandeza de nuestro mundo.";
 
+            string r = CensorWord.CensorWords(phrase, ["mundo", "lugar", "belleza"]);
+
+            Console.WriteLine(r);
+            
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine(phrase);
 
             /// Dont delete this line
             Console.ReadLine();
---
+++ Solutions/CensorWord.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Logica.Solutions
+{
+    public class CensorWord
+    {
+        static public string CensorWords(string? phrase, string? wordToCensor)
+        {
+            if (phrase == null && wordToCensor == null) return "You cannot read the phrase and the searched word";
+
+            if (phrase == null) return $"You cannot read the phrase.\nSearched Word: {wordToCensor}";
+
+            if(wordToCensor == null) return phrase;
+
+            string regex = @"\b" + Regex.Escape(wordToCensor) + @"\b";
+
+            string censoredPhrase = Regex.Replace(phrase.ToLower(), regex, "[-CENSORED-]");
+
+            return censoredPhrase;
+        }
+
+        static public string CensorWords(string? phrase, List<string>? wordsToCensor)
+        {
+            if (phrase == null && wordsToCensor == null) return "You cannot read the phrase and the searched word";
+
+            if (phrase == null) return $"You cannot read the phrase.\nSearched Words: {wordsToCensor}";
+
+            if (wordsToCensor == null || wordsToCensor.Count == 0) return phrase;
+
+            string result = phrase;
+
+            for (int i = 0; i < wordsToCensor.Count; i++)
+            {
+                string regex = @"\b" + Regex.Escape(wordsToCensor[i]) + @"\b";
+                result = Regex.Replace(result.ToLower(), regex, "[-CENSORED-]");
+                
+            }
+
+            return result;
+        }
+    }
+}
