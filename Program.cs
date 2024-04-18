74d02ce389948b351bf503ea647b88be368c924e
MMorales <mmorales@iparksa.com>
2024-04-18 22:08:41 +0200

    Decending by 8

--- Program.cs
+++ Program.cs
@@ -6,15 +6,7 @@ namespace Logica
     {
         static void Main(string[] args)
         {
-            string phrase = "En este vasto mundo, cada rincón es un lugar único lleno de misterios. Explorar el mundo es sumergirse en una aventura sin fin, donde cada lugar guarda secretos sorprendentes. El mundo es nuestro hogar, un lugar maravilloso lleno de diversidad y belleza. No importa dónde vayas, siempre encontrarás algo nuevo por descubrir en este increíble mundo. Cada cultura, cada paisaje, cada lugar contribuye a la increíble variedad que hace que nuestro mundo sea tan fascinante. Así que sal y explora este maravilloso mundo, porque cada lugar que visites te llevará a una nueva experiencia y te recordará la grandeza de nuestro mundo.";
-
-            string r = CensorWord.CensorWords(phrase, ["mundo", "lugar", "belleza"]);
-
-            Console.WriteLine(r);
-            
-            Console.WriteLine("\n\n");
-
-            Console.WriteLine(phrase);
+            Decending.DecendingInN(100, 8);
 
             /// Dont delete this line
             Console.ReadLine();
---
+++ Solutions/Decending.cs
@@ -0,0 +1,28 @@
+﻿namespace Logica.Solutions
+{
+    public class Decending
+    {
+
+        public static void DecendingInN(int number, int decenderN)
+        {
+            string result = string.Empty;
+
+            int currentValue = number;
+
+            for (int i = 0; i <= number ; i++)
+            {
+                result += $"- nº{currentValue}\n";
+                currentValue -= decenderN;
+
+                if (currentValue < 0)
+                {
+                    result += "---FIN---";
+                    break;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+
+    }
+}
